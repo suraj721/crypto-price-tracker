4eb090a41e0f86407321ebfda3cb1d567141d757
Suraj Kumar Rai <surajkumarrai@static-50-50-49-72.waus.wi.frontiernet.net>
2025-04-23 13:04:35 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <img src='https://i.pinimg.com/474x/9b/a4/81/9ba4810782e91f22c1baa0cdd93ab8e5.jpg' alt='img'></img>
+    </div>
+  );
+}
+
+export default App;
